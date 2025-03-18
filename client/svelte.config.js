bfedf15f4b3fe39c3fadc91ea245aeb256cacf6c
Brendan Muhia <dotcombrendan@gmail.com>
2025-03-18 14:55:05 +0300

    Fix routing configuration in svelte.config.js to support custom routing

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+	  // Disable SvelteKit's built-in routing as we're using svelte-routing
+	  router: false,
+	  // Disable SvelteKit's built-in hydration as we're using our own
+	  hydrate: false
 	}
-};
-
-export default config;
+  };
+  
+  export default config;
